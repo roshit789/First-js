bbf39760a7faef198aff8d7dd0ce3750e2566ade
Roshit Shreesh <123299645+roshit789@users.noreply.github.com>
2024-07-22 01:33:29 +0000

    container config

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,8 @@
+// let name = "Roshit"
+// let age = 15
+// let isLogginIn = false
+
+// let state = null
+
+
+console.log(typeof "Roshit");
---
+++ 01_basic/03_variable.js
@@ -0,0 +1,11 @@
+const accountId = 14423
+let accountEmail = "roshit@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pokhara"
+// accountId = 2 // not allowed because constant is unchangable
+
+accountEmail = "Hello@gmail.com"
+accountPassword = "Hello12345"
+accountCity = "Kathmandu"
+
+console.log([accountId, accountEmail, accountPassword, accountCity]);
