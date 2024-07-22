318cc5c8ceb0db47f5892deb77013a3928261d8d
Roshit Shreesh <123299645+roshit789@users.noreply.github.com>
2024-07-22 01:56:37 +0000

    Conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,27 @@
+//1
+// let score = undefined
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//2
+let isLoggedIn = "Roshit"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//Note
+// 1 => true 0 => false
+// "" => false
+// "Roshit" => True
+
+
+//3
+let someNumber = 123
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
