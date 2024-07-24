4daba67a7ef26031b0179602828f42004ecb562f
Roshit Shreesh <123299645+roshit789@users.noreply.github.com>
2024-07-24 16:10:34 +0000

    operation and conversion

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -23,5 +23,35 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 123
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************Operations****************************
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2**3);
+// console.log(2%3);
+
+//String add 
+let str1 = "Roshit"
+let str2 = " Shreesh"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+
+//different one
+//console.log("1" + 2 +2); //string first ma huda sabai lai string nai manchha javasccript le
+//console.log(3 + 2 + "1"); // String last ma chha vane pahile conversion hunchha 
+
+
+//Tricky conversion
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
